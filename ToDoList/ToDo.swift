e3622c753ef8a44155007334294b7d4daf0754be
Keegan Ryan <keegan.ryan.edu@gmail.com>
2020-06-08 13:37:19 -0400

    hopefully this uploads to GitHub

---
+++ ToDoList/ToDo.swift
@@ -0,0 +1,14 @@
+//
+//  ToDo.swift
+//  ToDoList
+//
+//  Created by Keegan Ryan on 6/8/20.
+//  Copyright © 2020 Keegan Ryan. All rights reserved.
+//
+
+import UIKit
+
+class ToDo {
+    var name = ""
+    var important = false
+}
